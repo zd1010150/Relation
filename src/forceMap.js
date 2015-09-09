53b93c5051aa454b2655078d517f1622e447284d
毛姆 <fedeoo.zf@alibaba-inc.com>
2015-09-09 22:05:07 +0800

    d3

---
+++ src/forceMap.js
@@ -0,0 +1,187 @@
+'use strict';
+
+function ForceMap(options) {
+    var container = options.container || document.body,
+    width = options.width || 960,
+    height = options.height || 600;
+    var nodesData = options.nodes; // distinguish between data and svn dom node
+    var linksData = options.links;
+    var nodeMap = {};
+    nodesData.forEach(function (item, index) {
+        nodeMap[item.id] = item;
+        item._index = index;
+    });
+    linksData.forEach(function (item) {
+        item.sourceId = item.source;
+        item.targetId = item.target;
+        item.source = nodeMap[item.sourceId]._index;
+        item.target = nodeMap[item.targetId]._index;
+    });
+
+    if (nodesData.length === 0) {
+        return ;
+    }
+
+    var svg = d3.select(container).select('svg').select('g');
+    if (svg.size() === 0) {
+        svg = d3.select(container).append('svg')
+        .attr('width', width)
+        .attr('height', height)
+        .append('g');
+    }
+
+    nodesData[0].fixed = true;
+    nodesData[0].x = width / 2;
+    nodesData[0].y = height / 2;
+    var force = d3.layout.force()
+        .nodes(nodesData)
+        .links(linksData)
+        // .gravity(0.05)
+        .linkDistance(120)
+        .charge(-150)
+        .size([width, height])
+        .start();
+    // paint links first so that put link under the node, link will not trigger mouse event
+    var links = svg.selectAll('.link')
+        .data(linksData)
+        .enter()
+        .append('line')
+        .attr('class', function(d) {
+            var classNames = ['link'];
+            if (!d.directed) {
+                classNames.push('dotted');
+            }
+            return classNames.join(' ');
+        })
+        .append('text')
+        .text(function (d) {
+            return d.name;
+        });
+
+    var nodes = svg.selectAll('.node')
+        .data(nodesData)
+        .enter()
+        .append('g')
+        .attr('class', function (d) {
+            var classNames = ['node'];
+            return classNames.join(' ');
+        })
+        .call(force.drag)
+        .on('mouseover', function (d) {
+            hightlightLinked(d.id);
+        })
+        .on('mouseout', function (d) {
+            clearHighligt();
+        });
+    // 插入 pattern
+    var defs = svg.append('svg:defs')
+        .selectAll('.pattern')
+        .data(nodesData)
+        .enter()
+        .append('svg:pattern')
+        .attr('id', function (d) {
+            return 'pattern_' + d.id;
+        })
+        .attr('width', '40')
+        .attr('height', '40')
+        .append('svg:image')
+        .attr('xlink:href', function (d) {
+            return d.avatarUrl;
+        })
+        .attr('x', 0)
+        .attr('y', 0)
+        .attr('width', 40)
+        .attr('height', 40);
+
+    nodes.append("circle")
+        .attr("xlink:href", function (d) {
+            return d.avatarUrl;
+        })
+        .attr("x", 0)
+        .attr("y", 0)
+        .attr("r", 20)
+        .style("fill", function (d) {
+            return 'url(#' + 'pattern_' + d.id + ')';
+        });
+
+    nodes.append('text')
+        .attr('dx', '-1em')
+        .attr('y', '3em')
+        .text(function(d) {
+            return d.name;
+        });
+
+    force.on("tick", function() {
+        links.attr("x1", function(d) {
+            return d.source.x;
+        })
+        .attr("y1", function(d) {
+            return d.source.y;
+        })
+        .attr("x2", function(d) {
+            return d.target.x;
+        })
+        .attr("y2", function(d) {
+            return d.target.y;
+        });
+
+        nodes.attr("transform", function(d) {
+            return "translate(" + d.x + "," + d.y + ")";
+        });
+    });
+
+    /**
+     * [hightlightLinked 高亮相邻节点和连续]
+     * @param  {[String]} id [节点id]
+     */
+    function hightlightLinked (id) {
+        var linedNodes = findLinkedNodesById(id);
+        linedNodes.forEach(function (item) {
+            item.involed = true;
+        });
+        render();
+    }
+
+    function clearHighligt () {
+        nodesData.forEach(function (item) {
+            item.involed = false;
+        });
+        linksData.forEach(function (item) {
+            item.involed = false;
+        });
+        render();
+    }
+
+    function render () {
+        svg.selectAll('.node')
+            .attr('class', function(d) {
+                var classNames = ['node'];
+                if (d.involed) {
+                    classNames.push('involed');
+                }
+                return classNames.join(' ');
+            });
+        svg.selectAll('.link')
+            .attr('class', function(d) {
+                var classNames = ['link', 'dotted'];
+                if (d.involed) {
+                    classNames.push('involed');
+                }
+                return classNames.join(' ');
+            });
+    }
+
+    function findLinkedNodesById (id) {
+        return linksData.reduce(function (ret, item) {
+            if (item.sourceId === id) {
+                ret.push(nodeMap[item.targetId]);
+                item.involed = true;
+            } else if (item.targetId === id){
+                 ret.push(nodeMap[item.sourceId]);
+                 item.involed = true;
+            }
+            return ret;
+        }, []);
+    }
+
+};
---
+++ src/index.js
@@ -0,0 +1,67 @@
+'use strict';
+(function () {
+
+})();
+
+// avatarUrl: https://work.alibaba-inc.com/photo/103313.120x120.jpg
+// avatarUrl: https://work.alibaba-inc.com/photo/103067.120x120.jpg
+var nodes = [{
+    id: '103313',
+    name: '藤椒',
+    avatarUrl: 'https://github.com/favicon.ico'
+}, {
+    id: '103067',
+    name: '花椒',
+    avatarUrl: 'https://github.com/favicon.ico'
+}, {
+    id: '103568',
+    name: '八角',
+    avatarUrl: 'https://github.com/favicon.ico'
+}, {
+    id: '103569',
+    name: '茴香',
+    avatarUrl: 'https://github.com/favicon.ico'
+}, {
+    id: '77164',
+    name: '大料',
+    avatarUrl: 'https://github.com/favicon.ico'
+}, {
+    id: '103854',
+    name: '胡椒',
+    avatarUrl: 'https://github.com/favicon.ico'
+}];
+
+nodes.forEach(function (item) {
+    if (item.id) {
+        item.avatarUrl = 'https://work.alibaba-inc.com/photo/' + item.id + '.120x120.jpg';
+    }
+});
+
+var links = [{
+    source: '103313',
+    target: '103067',
+    name: '就不告诉你'
+}, {
+    source: '103313',
+    target: '103854',
+    name: '就不告诉你'
+}, {
+    source: '103067',
+    target: '103568',
+    name: '就不告诉你'
+}, {
+    source: '103568',
+    target: '103569',
+    name: '就不告诉你'
+}, {
+    source: '77164',
+    target: '103854',
+    name: '就不告诉你'
+}];
+
+var options = {
+    nodes: nodes,
+    links: links,
+};
+
+ForceMap(options);
